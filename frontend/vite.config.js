4c472b92e1f3ed0afb02102e3823e6eccc70de82
derian-c <magicalhippo77@gmail.com>
2025-02-17 20:28:19 -0600

    Remove frontend proxy and make requests to backend directly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //here we set up a proxy so all /api calls go to our flask backend
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.BACKEND_URL,
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 });

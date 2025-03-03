7eae794dc147b7ab9a8cd3c40da5e001c02d7b72
derian-c <magicalhippo77@gmail.com>
2025-03-02 22:15:11 -0600

    Add all CRUD operations for physicians and patients

--- backend/controllers/physician.py
+++ backend/controllers/physician.py
@@ -14,15 +14,42 @@ def get_physicians():
 @physicians.route('/<int:id>', methods=['GET'])
 def get_physician(id):
   physician = db.session.get(Physician, id)
-  return jsonify(physician.dict())
+  if physician:
+    return jsonify(physician.dict())
+  return jsonify({'error': 'Physician does not exist'}), 422
+
+# Update info for one patient by id
+@physicians.route('/<int:id>', methods=['PUT'])
+def update_patient(id):
+  physician = db.session.get(Physician, id)
+  if physician:
+    data = request.get_json()
+    physician.name = data.get('name')
+    physician.email_address = data.get('email_address')
+    db.session.commit()
+    return jsonify(physician.dict())
+  return jsonify({'error': 'Physician does not exist'}), 422
 
 # Create a physician with a name and email address
-@physicians.route('/create', methods=['POST'])
+@physicians.route('/', methods=['POST'])
 def create_physician():
   data = request.get_json()
   name = data.get('name')
   email_address = data.get('email_address')
+  physician = db.session.scalars(db.select(Physician).filter_by(name=name,email_address=email_address)).first()
+  if physician:
+    return jsonify({'error': 'Physician already exists'}), 422
   physician = Physician(name=name,email_address=email_address)
   db.session.add(physician)
   db.session.commit()
   return jsonify(physician.dict())
+
+# Delete physician by id
+@physicians.route('/<int:id>/delete', methods=['DELETE'])
+def delete_physician(id):
+  physician = db.session.get(Physician, id)
+  if physician
+    db.session.delete(physician)
+    db.session.commit()
+  else:
+    return jsonify({'error': 'Physician does not exist'}), 422

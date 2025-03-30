815e98d3b2beeb2479d18143e9d77dc8a8135ed7
derian-c <magicalhippo77@gmail.com>
2025-03-30 15:10:26 -0500

    Fetch data from backend correctly

--- frontend/src/pages/AdminView.jsx
+++ frontend/src/pages/AdminView.jsx
@@ -3,7 +3,7 @@ import { isAdmin } from "../apis/isAdmin";
 import { getUsersRole } from "../apis/getUserRole";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
-import { getPhysicians } from '@/apis/physicianService'
+import { getPhysicians, createPhysician, deletePhysicianByID } from '@/apis/physicianService'
 import { getAdmins } from '@/apis/adminService'
 import useFetchProtectedData from "../utils/fetchFromApi";
 
@@ -58,7 +58,6 @@ function AdminView() {
         const accessToken = await getAccessTokenSilently();
         const outPutRoles = getUsersRole(user, accessToken);
         setRoles(outPutRoles);
-        console.log(outPutRoles);
         const permission = isAdmin(outPutRoles);
         permission ? setCanEnter(true) : navigate("/");
       } catch (error) {
@@ -163,19 +162,12 @@ function AdminView() {
   const handleInviteSubmit = async (e) => {
     e.preventDefault();
     try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch("/physicians/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          first_name: inviteFirstName,
-          last_name: inviteLastName,
-          email_address: inviteEmail,
-        }),
-      });
+      const physicianData = {
+        first_name: inviteFirstName,
+        last_name: inviteLastName,
+        email_address: inviteEmail,
+      }
+      const response = await createPhysician(physicianData);
 
       const responseText = await response.text();
 
@@ -231,11 +223,7 @@ function AdminView() {
       return;
 
     try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch(`/physicians/${id}`, {
-        method: "DELETE",
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const response = await deletePhysicianByID(id)
 
       if (!response.ok) {
         const errorData = await response.json();
